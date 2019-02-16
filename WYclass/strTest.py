6d7e9186a3754dcc363a4d94669c8b73cf016400
shengjia <shengj@anchnet.com>
2019-02-16 14:20:41 +0800

    字符串

---
+++ WYclass/strTest.py
@@ -0,0 +1,7 @@
+a = "My first test string"
+b = 'another test string that i have defined'
+c = "This is Sal's string"
+d = 'this is Sal\'s string'
+e = 'My favorite word is "zoe",what is yours?'
+math_string = "3+4*2"
+expression_string = "a+' '+b+' tiger'"
---
+++ WYclass/test1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    @staticmethod
+    def detectCapitalUse(word):
+        c = 0
+        for i in word:
+            if i == i.upper():
+                c += 1
+        return c == len(word) or (c == 1 and word[0] == word[0].upper()) or c == 0
+    word = input("Enter:")
+    detectCapitalUse(word)
