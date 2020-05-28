d964af05ef128fd753450e5f8deb9fdbe866ba3d
盛佳 <1368329355@qq.com>
2020-05-28 14:42:20 +0800

    [ head first in python ] update the nester with new param fn

--- HeadFirstInPython/nester/nester.py
+++ HeadFirstInPython/nester/nester.py
@@ -1,13 +1,16 @@
 # @File    : nester.py
 # @Date    : 2020-05-26
 # @Author  : shengjia
+import sys
 
-def print_lol(the_list, level):
+
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
     for name in the_list:
         if isinstance(name, list):
-            print_lol(name, level + 1)
+            print_lol(name, indent, level + 1, fn)
 
         else:
-            for tab_stop in range(level):
-                print("\t")
-            print(name)
+            if indent:
+                for tab_stop in range(level):
+                    print("\t")
+                print(name)
