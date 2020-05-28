f14f27b88fd02c42f1ff627fcddcec3f0db4c2aa
盛佳 <1368329355@qq.com>
2020-05-28 14:17:04 +0800

    [ learn ]

---
+++ learn_python_basic/fun_package_module.py
@@ -0,0 +1,4 @@
+# @File    : fun_package_module.py
+# @Date    : 2020-02-17
+# @Author  : zoe
+
---
+++ learn_python_basic/variable.py
@@ -0,0 +1,7 @@
+# @File    : variable.py
+# @Date    : 2020-02-15
+# @Author  : zoe
+a = 1
+b = a
+a = 2
+print(a)
