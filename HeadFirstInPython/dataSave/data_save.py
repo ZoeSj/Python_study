ee22d1e6533b524d59128075fdcba8712f7f6983
盛佳 <1368329355@qq.com>
2020-05-28 14:43:26 +0800

    [ head first in python ] save data with file

---
+++ HeadFirstInPython/dataSave/data_save.py
@@ -0,0 +1,44 @@
+# @File    : data_save.py
+# @Date    : 2020-05-28
+# @Author  : shengjia
+
+man = []
+other = []
+try:
+    data = open('sketch.txt')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The data file is missing')
+
+try:
+    man_file = open('man_data.txt', 'w')
+    other_file = open('other_data.txt', 'w')
+
+    print(man)
+    print(other)
+
+    man_file.close()
+    other_file.close()
+except IOError as err:
+    print('file error' + str(err))
+finally:
+    if 'data' in locals():
+        data.close()
+
+try:
+    with open('man_data.txt', 'w') as man_file:
+        print(man)
+    with open('other_data.txt', 'w') as other_file:
+        print(other)
+except IOError as err:
+    print('file error' + str(err))
